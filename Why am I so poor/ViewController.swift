5b8b59f0d1472bc1edd442e33db9a80a71716d69
Grace, Mu-Hui Yu <muhuiyu.grace@gmail.com>
2022-07-03 14:08:39 +0800

    Initial Commit

---
+++ Why am I so poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Why am I so poor
+//
+//  Created by Mu Yu on 7/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
